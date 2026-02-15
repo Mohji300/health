3ffab25116295e165d9081b05113b1b6f74bbc62
carlo <ramirezjhncrl@gmail.com>
2026-02-15 12:44:26 +0800

    fixed so that other computers can access the site

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/SHN/';
+$config['base_url'] = (isset($_SERVER['HTTP_HOST']))
+	? ((isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] == 1)) ? 'https' : 'http')
+		. '://' . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\') . '/'
+	: 'http://localhost/SHN/';
 
 /*
 |--------------------------------------------------------------------------
