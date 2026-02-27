3edf8b9210f50f4ab2b76ef9dafb8154d186dc7e
carlo <ramirezjhncrl@gmail.com>
2026-02-27 18:18:58 +0800

    changed the name to Nutritionalassessment

--- application/controllers/NutritionalAssessment.php
+++ application/controllers/NutritionalAssessment.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class nutritionalassessment extends CI_Controller {
+class Nutritionalassessment extends CI_Controller {
 
     public function __construct()
     {
