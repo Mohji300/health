5ccafed06eaa74edd41228170beb6cf7c0863968
carlo <ramirezjhncrl@gmail.com>
2026-02-27 18:16:55 +0800

    changed the name to Division_reports_controller

--- application/controllers/Division_reports_controller.php
+++ application/controllers/Division_reports_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class division_reports_controller extends CI_Controller {
+class Division_reports_controller extends CI_Controller {
 
     public function __construct()
     {
