ccb2ca2f9d69b879db32392036a3eaf0294c0f3d
carlo <ramirezjhncrl@gmail.com>
2026-02-27 18:16:13 +0800

    hange the name to District_reports_controller

--- application/controllers/District_reports_controller.php
+++ application/controllers/District_reports_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class district_reports_controller extends CI_Controller {
+class District_reports_controller extends CI_Controller {
 
     public function __construct()
     {
