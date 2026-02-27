bf25c937e375efa8d2b251dcf1a3940a98bc87bb
carlo <ramirezjhncrl@gmail.com>
2026-02-27 18:19:43 +0800

    changed the name to Sbfp_beneficiaries_controller

--- application/controllers/sbfp_beneficiaries_controller.php
+++ application/controllers/sbfp_beneficiaries_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class sbfp_beneficiaries_controller extends CI_Controller {
+class Sbfp_beneficiaries_controller extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
