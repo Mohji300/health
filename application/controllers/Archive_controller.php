d74a70280765c696e8719ac2e9a73555169a1e04
carlo <ramirezjhncrl@gmail.com>
2026-02-27 18:14:50 +0800

    change the name to Archive_controller

--- application/controllers/Archive_controller.php
+++ application/controllers/Archive_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class archive_controller extends CI_Controller {
+class Archive_controller extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
