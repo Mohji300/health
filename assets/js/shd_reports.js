6ce9ca3eb5e1640c59989c938b11a9033f8b9d56
carlo <ramirezjhncrl@gmail.com>
2026-06-15 11:03:39 +0800

    removed assets outside the application folder

--- assets/js/shd_reports.js
+++
@@ -1,7 +0,0 @@
-// SHD reports JavaScript (placeholder)
-$(function(){
-  // Example: initialize simple interactions
-  $('#shdReportsTable').on('click', 'tr', function(){
-    $(this).toggleClass('table-active');
-  });
-});
